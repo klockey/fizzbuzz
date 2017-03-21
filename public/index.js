0ae2dfc232d9247346d5fb888187253da294c731
Keith Lockey <lockey@gmail.com>
2017-03-21 11:32:37 -0400

    added %

--- public/index.js
+++ public/index.js
@@ -1,11 +1,11 @@
 for (let i = 1; i < 101; i++) {
-  if (i % 3 === 0) {
+  if ((i % 3 === 0) && (i % 5 === 0)) {
+    console.log('FizzBuzz')
+  } else if (i % 3 === 0) {
     console.log('Fizz')
   } else if (i % 5 ===0) {
     console.log('Buzz')
-  } else if ((i / 3 ===0) && (i / 5 === 0)) {
-    console.log('FizzBuzz')
-  } else {
+  }  else {
     console.log(i)
   }
 }
